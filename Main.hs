c8519bc3b9164de6618c978ef241cc9b68a87065
uuhan <xuminhui189@gmail.com>
2018-09-23 22:48:09 +0800

    add proxy port

--- Main.hs
+++ Main.hs
@@ -129,7 +129,7 @@ fetchIds = do
   where
     url = "https://hacker-news.firebaseio.com/v0/topstories.json"
     opts = defaults
-            -- & proxy ?~ httpProxy "localhost" 1111
+            & proxy ?~ httpProxy "localhost" 1111
             -- & manager .~ Left defaultManagerSettings { managerResponseTimeout = responseTimeoutDefault }
 
 fetchStoryById :: Int -> IO Story
